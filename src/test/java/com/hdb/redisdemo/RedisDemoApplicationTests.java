82f748dd4cda319e6699209a550635f01c52dcd7
hollis <herryhollis@163.com>
2024-01-18 19:39:28 +0800

    测试存储string，对象

--- src/test/java/com/hdb/redisdemo/RedisDemoApplicationTests.java
+++ src/test/java/com/hdb/redisdemo/RedisDemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.hdb.redisdemo;
 
+import com.hdb.redisdemo.pojo.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,13 +10,21 @@ import org.springframework.data.redis.core.RedisTemplate;
 class RedisDemoApplicationTests {
 
     @Autowired
-    private RedisTemplate redisTemplate;
+    private RedisTemplate<String, Object> redisTemplate;
 
     @Test
     void contextLoads() {
         redisTemplate.opsForValue().set("name", "彬哥");
         String name = (String) redisTemplate.opsForValue().get("name");
         System.out.println("name = " + name);
+    }
+
+    @Test
+    void testSaveUser() {
+        redisTemplate.opsForValue().set("user:100", new User("彬哥", 23));
+
+        User user = (User) redisTemplate.opsForValue().get("user:100");
+        System.out.println("user = " + user);
     }
 
 }
